242e6f7549b4b86f86e02175b2f468398a8517bf
Emre <aka548548@gmail.com>
2022-09-24 18:19:05 +0300

    challenge 3

---
+++ Challenge_3/Program.cs
@@ -0,0 +1,26 @@
+﻿List<int> s = new() { 1, 2, 1, 3, 2, };
+int d = 3;
+int m = 2;
+
+int total = 0;
+
+int totalTemp = 0;
+for (var i = 0; i < s.Count(); i++)
+{
+    totalTemp = 0;
+    if ((i + m) > s.Count())
+        break;
+
+    List<int> tempArray = s.Skip(i).Take(m).ToList();
+    for (var j = 0; j < tempArray.Count(); j++)
+    {
+        totalTemp += tempArray[j];
+    };
+    if (totalTemp == d)
+    {
+        total += 1;
+    }
+};
+
+//return total;
+Console.WriteLine(total);
